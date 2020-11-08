58a3c540096c49b103057e7e1836083fa79c144e
huangtao <510387708@qq.com>
2020-11-08 15:12:47 +0800

    first

--- src/main/java/com/example/community/controller/HelloController.java
+++ src/main/java/com/example/community/controller/HelloController.java
@@ -12,6 +12,6 @@ public class HelloController {
     public String hello(@RequestParam(name = "name") String name, Model model){
         model.addAttribute("name",name);
         System.out.println("1输出");
-        return "hello";
+        return "index";
     }
 }
